7d5d34e31b1dd4cc3046cdeb994819ceaa70e93c
thisishoon <jihoon522@naver.com>
2020-03-05 13:21:05 +0900

    dfs을 이용한 조합

---
+++ 1182.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int n, s;
+int arr[22];
+int result;
+
+void dfs(int index, int sum, int length){
+    
+    //if(index==n){
+        if(length>0){
+            cout<<index<<" "<<sum<<endl;
+            if(sum==s) result++;
+        }
+        //return;
+    //}
+    
+    dfs(index+1, sum+arr[index+1], length+1);
+    dfs(index+1, sum, length);
+}
+
+
+int main(){
+    cin>>n>>s;
+    for(int i=1; i<=n; i++) cin>>arr[i];
+    dfs(0, 0, 0);
+    cout<<result<<endl;
+    return 0;
+}
--- 14225.cpp
+++ 14225.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 int n;
 int arr[22];
-int main(){
-    cin>>n
-    for(int i=1; i<=n; i++) cin>>arr[i];
+int result;
+vector<int> v;
 
-    
+void dfs(int index, int sum){
+    if(index>n){
+        v.push_back(sum);
+        return;
+    }
+    dfs(index+1, sum+arr[index]);
+    dfs(index+1, sum);
+}
+
+int main(){
+    cin>>n;
+    for(int i=1; i<=n; i++) cin>>arr[i];
+    dfs(1,0);
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    int result=1;
+    for(int i=1; i<v.size(); i++){
+        if(v[i]!=result){
+            cout<<result<<endl;
+            return 0;
+        }
+        else{
+            result++;
+        }
+    }
+    cout<<result<<endl;
 
     return 0;
 }
