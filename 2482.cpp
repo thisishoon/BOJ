11db1821192d36144e4e9b09a2a3961a0dac9076
thisishoon <jihoon522@naver.com>
2020-05-21 15:03:45 +0900

    원을 선형적으로 풀어 계산하는 DP

--- 2482.cpp
+++ 2482.cpp
@@ -4,26 +4,25 @@
 using namespace std;
 int n, k;
 int answer;
-int arr[1001];
+int dp[1001][1011];     //dp[a][b] = a까지 b개를 선택하는 경우의 수
 const int mod = 1000000003;
-
-void combination(int num, int last){
-    if(num==k){
-        if(arr[0]==1 && arr[k-1]==n)  return;
-
-        answer= (answer+1)%mod;
-        return;
-    }
-    for(int i=last; i<=n; i++){
-        arr[num]=i;
-        combination(num+1, i+2);
-        arr[num]=0;
-    }
-}
 
 int main(){
     cin>>n>>k;
-    combination(0, 1);
+    for(int i=1; i<=n; i++){
+        dp[i][1]=i;
+    }    
+    if(k==1){
+        cout<<n<<endl;
+        return 0;
+    }
+
+    for(int i=2; i<=n; i++){
+        for(int j=2; j<=k; j++){
+            dp[i][j] = (dp[i-2][j-1] + dp[i-1][j])%mod;
+        }
+    }
+    int answer = (dp[n-3][k-1] + dp[n-1][k]) % mod;
     cout<<answer<<endl;
 
     return 0;
