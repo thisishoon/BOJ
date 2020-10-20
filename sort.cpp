e14981fbfcdbbb3808e6afa55e8ea6456baa6b0e
thisishoon <jihoon522@naver.com>
2020-10-20 19:24:40 +0900

    bubble, merge sort 구현

--- sort.cpp
+++ sort.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void selectionSort(int *arr, int len){
+void selectionSort(int *arr, int len){  //가장 작은 값은 선택
     for(int i=0; i<len-1; i++){
         int minIdx=i;
         for(int j=i+1; j<len; j++){
@@ -16,18 +16,61 @@ void selectionSort(int *arr, int len){
     }
 }
 
-void insertionSort(int *arr, int len){
+void insertionSort(int *arr, int len){  //key값이 들어갈 자리를 찾기까지 한 칸씩 밈
     for(int i=1; i<len; i++){
         int key = arr[i];
         int j;
-        for(j=i-1; j>=0 && arr[j]>key; j--){
+        for(j=i-1; j>=0 && arr[j]>key; j--){  
             arr[j+1] = arr[j];
         }
         arr[j+1] = key;
     }
 }
 
-void quickSort(int *arr, int start, int end){
+int sorted[10];
+
+void merge(int *arr, int left, int right){
+    int mid = (left+right)/2;
+    int i = left;
+    int j = mid+1;
+    int k = left;
+
+    while(i<=mid && j<=right){
+        if(arr[i] <= arr[j]){
+            sorted[k++] = arr[i++];
+        }
+        else{
+            sorted[k++] = arr[j++];
+        }
+    }
+
+    if(i>mid){
+        for(int t=j; t<=right; t++){
+            sorted[k++] = arr[t];
+        }
+    }
+    else{
+        for(int t=i; t<=mid; t++){
+            sorted[k++] = arr[t];
+        }
+    }
+
+    for(int t=left; t<=right; t++){
+        arr[t] = sorted[t];
+    }
+}
+
+void mergeSort(int *arr, int left, int right){  //분할 후 병합 과정에서 정렬
+    if(left<right){
+        int mid = (left+right)/2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid+1, right);
+        merge(arr, left, right);
+    }
+}
+
+
+void quickSort(int *arr, int start, int end){   //pivot을 기준으로 정렬한 뒤 분할
     if(start >= end){
         return;
     }
@@ -57,10 +100,24 @@ void quickSort(int *arr, int start, int end){
     quickSort(arr, j+1, end);
 }
 
+void bubbleSort(int* arr, int len){
+    for(int i=0; i<len-1; i++){
+        for(int j=i+1; j<len; j++){
+            if(arr[i]>arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
 int main(){
     int arr[10] = {1100,123,36,1,78,44,2,43,99,11111};
     // quickSort(arr, 10);
-    insertionSort(arr, 10);
+    // insertionSort(arr, 10);
+    // mergeSort(arr, 0, 9);
+    bubbleSort(arr, 10);
     for(int i=0; i<10; i++){
         cout<<arr[i]<<" ";
     }
